fe54c7560e5b974da5b3edbf41ec4d64ee29746b
p-yamini8 <yaminifreez1@gmail.com>
2025-08-29 12:02:28 +0530

    login backend

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,8 @@
 const User=require('../model/user');
 const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+require('dotenv').config();
 exports.signup=async(req,res)=>{
     try{
 const {name,email,phone,password}=req.body;
@@ -23,5 +26,35 @@ catch(err)
 {
     console.log(err)
     res.status(500).json({message:'error signup',err})
+}
+}
+
+exports.login=async(req,res)=>{
+    const {email,password}=req.body;
+    if(!email||!password)
+    {
+        return res.status(400).json('missing fields');
+
+    }
+    try{
+    const user=await User.findOne({where:{email}});
+    if(!user)
+    {
+        return res.status(404).json({message:'User not found'})
+    }
+    if(!(await bcrypt.compare(password,user.password)))
+    {
+return res.status(401).json({message:'Invalid credentials'});
+    }
+    const token=jwt.sign({id:user.id,
+        name:user.name,
+        email:user.email,
+    },process.env.JWT_SECRET);
+    res.json({message:'Login successful',token,name:user.name,email:user.email})
+
+}catch(err)
+{
+    console.log(err);
+    return res.status(500).json({message:'error login',err})
 }
 }
---
+++ view/login/login.js
@@ -0,0 +1,23 @@
+const loginform=document.getElementById('login_form');
+loginform.addEventListener('submit',async(e)=>{
+    e.preventDefault();
+    alert('working')
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+ 
+        const res=await fetch('/user/login',{
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({email,password})
+        });
+        const result=await res.json();
+        if(res.ok)
+        {
+            loginform.reset();
+            alert('login success');
+        }
+    else{
+    alert(result.message||'login failed');
+
+    }
+})
